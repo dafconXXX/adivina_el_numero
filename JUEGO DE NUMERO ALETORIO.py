e025485310961a7c50b5a92d146c91b70913d66e
Dafcon <danielfelipebc11@gmail.com>
2021-02-03 17:48:58 -0500

    la primera version del juego del adivinar el numero

---
+++ JUEGO DE NUMERO ALETORIO.py
@@ -0,0 +1,17 @@
+import random
+
+def run():
+    numero_aleotorio = random.randint(1, 10)
+    numero_elegido = int(input("Ingrese un numero: "))
+    while numero_elegido != numero_aleotorio:
+        if numero_aleotorio > numero_elegido:
+            print('El numero buscado es mayor')
+        else:
+            print('El numero buscado es mas pequeño')
+        numero_elegido = int(input("Ingrese otro numero: "))
+    print("Ganaste!!!")
+
+
+
+if __name__ == "__main__":
+    run()
